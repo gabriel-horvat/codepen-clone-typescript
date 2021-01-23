dea30de51f8bb9e131ddc60b8f6316b81f752da9
Gabmeister <gabriel@gabrielhorvat.com>
2021-01-23 16:19:52 +0100

    basic app component

--- codepen-clone/src/App.tsx
+++ codepen-clone/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>CODEPEN CLONE</h1>
+      <div>
+        <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <button onClick={onClick}>submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
-}
+};
 
 export default App;
