94520dd7004b7c991c0f02ed83c9f31ded6ebae5
Gabriel <gabriel@gabrielhorvat.com>
2021-05-26 21:04:49 +0200

    config editor js

--- codepen-clone/src/components/code-editor.tsx
+++ codepen-clone/src/components/code-editor.tsx
@@ -1,7 +1,7 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" />;
+  return <MonacoEditor height="500px" language="javascript" />;
 };
 
 export default CodeEditor;
