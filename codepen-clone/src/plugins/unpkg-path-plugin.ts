97329c8ef24011d8cd323868b97da9300f5ce859
Gabriel <gabriel@gabrielhorvat.com>
2021-06-09 15:34:18 +0200

    execute npm pkg WIP

--- codepen-clone/src/plugins/unpkg-path-plugin.ts
+++ codepen-clone/src/plugins/unpkg-path-plugin.ts
@@ -5,7 +5,15 @@ export const unpkgPathPlugin = () => {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path === "tiny-test-pkg") {
+          return {
+            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+            namespace: "a",
+          };
+        }
         return { path: args.path, namespace: "a" };
       });
 
@@ -16,14 +24,9 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+              import message from 'tiny-test-pkg';
               console.log(message);
             `,
-          };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
           };
         }
       });
