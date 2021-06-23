9d84d2ec5313edfe881253803cf44fd0251a7a8f
Gabriel <gabriel@gabrielhorvat.com>
2021-06-23 18:45:54 +0200

    refactor plugin

--- codepen-clone/src/plugins/unpkg-path-plugin.ts
+++ codepen-clone/src/plugins/unpkg-path-plugin.ts
@@ -15,7 +15,7 @@ const fileCache = localForage.createInstance({
   console.log(color);
 })();
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
@@ -47,10 +47,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === "index.js") {
           return {
             loader: "jsx",
-            contents: `
-            import React, {useState} from 'react';
-              console.log(React, useState);
-            `,
+            contents: inputCode,
           };
         }
 
