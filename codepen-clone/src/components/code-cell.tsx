2c40ae348320464f79fffca4457124ee1e60dee0
Gabriel <gabriel@gabrielhorvat.com>
2021-07-30 14:07:52 +0200

    execute user's code after 1sec automatically

--- codepen-clone/src/components/code-cell.tsx
+++ codepen-clone/src/components/code-cell.tsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CodeEditor from ".//code-editor";
 import Preview from ".//preview";
 import bundle from "../bundler";
 import Resizable from "./resizable";
 
 const CodeCell = () => {
+  // input is whatever the user is typing into editor
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    // only execute user's code after 1sec
+    const timer = setTimeout(async () => {
+      // bundle input
+      const output = await bundle(input);
+      // update code state
+      setCode(output);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
